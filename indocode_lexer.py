fcdc3ce5935503ba68640e10e57d3941eb90b582
gustasyaeful <62320170+gustasyaeful@users.noreply.github.com>
2020-06-11 20:42:24 +0700

    Add files via upload

--- indocode_lexer.py
+++ indocode_lexer.py
@@ -11,7 +11,7 @@ class BasicLexer(Lexer):
     THEN = r'MAKA'
     ELSE = r'LAINNYA'
     FOR = r'UNTUK'
-    FUN = r'FUN'
+    FUN = r'FUNGSI'
     TO = r'SAMPAI'
     ARROW = r'->'
     NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
@@ -21,7 +21,7 @@ class BasicLexer(Lexer):
 
     @_(r'\d+')
     def NUMBER(self, t):
-        t.value = float(t.value)
+        t.value = int(t.value)
         return t
 
     @_(r'#.*')
@@ -32,16 +32,3 @@ class BasicLexer(Lexer):
     def newline(self,t ):
         self.lineno = t.value.count('\n')
 
-
-if __name__ == '__main__':
-    lexer = BasicLexer()
-    env = {}
-    while True:
-        try:
-            text = input('indocode > ')
-        except EOFError:
-            break
-        if text:
-            lex = lexer.tokenize(text)
-            for token in lex:
-                print(token)
--- indocode_parser.py
+++ indocode_parser.py
@@ -1,37 +1,4 @@
-from sly import Lexer
 from sly import Parser
-
-class BasicLexer(Lexer):
-    tokens = { NAME, NUMBER, STRING, IF, THEN, ELSE, FOR, FUN, TO, ARROW, EQEQ }
-    ignore = '\t '
-
-    literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';' }
-
-    # Define tokens
-    IF = r'JIKA'
-    THEN = r'MAKA'
-    ELSE = r'LAINNYA'
-    FOR = r'UNTUK'
-    FUN = r'FUN'
-    TO = r'SAMPAI'
-    ARROW = r'->'
-    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    STRING = r'\".*?\"'
-
-    EQEQ = r'=='
-
-    @_(r'\d+')
-    def NUMBER(self, t):
-        t.value = float(t.value)
-        return t
-
-    @_(r'#.*')
-    def COMMENT(self, t):
-        pass
-
-    @_(r'\n+')
-    def newline(self,t ):
-        self.lineno = t.value.count('\n')
 
 class BasicParser(Parser):
     tokens = BasicLexer.tokens
@@ -111,16 +78,3 @@ class BasicParser(Parser):
     @_('NUMBER')
     def expr(self, p):
         return ('num', p.NUMBER)
-
-if __name__ == '__main__':
-    lexer = BasicLexer()
-    parser = BasicParser()
-    env = {}
-    while True:
-        try:
-            text = input('indocode > ')
-        except EOFError:
-            break
-        if text:
-            tree = parser.parse(lexer.tokenize(text))
-            print(tree)
